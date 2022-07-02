f77edf16c642ee912a5008c598eb8a519d2e321a
Taiju Sanagi <taijusanagi@gmail.com>
2022-07-03 03:00:44 +0900

    fix header link

--- packages/web/components/Header.tsx
+++ packages/web/components/Header.tsx
@@ -5,18 +5,18 @@ import { Link } from "./Link";
 
 export const Header: React.FC = () => {
   return (
-    <Link to="/">
-      <Text
-        mt="8"
-        align="center"
-        bgGradient="linear(to-tr, teal.300,yellow.400)"
-        bgClip="text"
-        fontSize="6xl"
-        fontWeight="extrabold"
-      >
+    <Text
+      mt="8"
+      align="center"
+      bgGradient="linear(to-tr, teal.300,yellow.400)"
+      bgClip="text"
+      fontSize="6xl"
+      fontWeight="extrabold"
+    >
+      <Link to="/">
         OverSoul <br />
         🔥
-      </Text>
-    </Link>
+      </Link>
+    </Text>
   );
 };
