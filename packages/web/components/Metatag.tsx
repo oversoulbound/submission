2106a3293d6521add00576edd095bf192de80599
Taiju Sanagi <taijusanagi@gmail.com>
2022-07-03 03:36:25 +0900

    ogp

---
+++ packages/web/components/Metatag.tsx
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import React from "react";
+
+export const Metatag: React.FC = () => {
+  return (
+    <Head>
+      <title>OverSoul 🔥</title>
+      <meta property="og:url" content="https://oversoul.vercel.app/" />
+      <meta property="og:title" content="OverSoul 🔥" />
+      <meta property="og:site_name" content="OverSoul 🔥" />
+      <meta property="og:description" content="Onchain reputation visualisation and credentials using SBTs" />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="https://oversoul.vercel.app/ogp.png" />
+      <meta name="twitter:card" content="summary_large_image" />
+    </Head>
+  );
+};
