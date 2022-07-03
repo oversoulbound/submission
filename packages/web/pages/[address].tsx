6175990327041c399a570a52514347a5d2c62d3a
Taiju Sanagi <taijusanagi@gmail.com>
2022-07-03 15:09:28 +0900

    added minter in web

--- packages/web/pages/[address].tsx
+++ packages/web/pages/[address].tsx
@@ -1,12 +1,13 @@
 import axios from "axios";
 import { ethers } from "ethers";
 import type { GetServerSideProps, NextPage } from "next";
-
+import { Flex, Text, VStack } from "@chakra-ui/react";
 import { Layout } from "../components/Layout";
 import { NFT } from "../types/nft";
-import { Helmet } from "react-helmet";
-import Iframe from "react-iframe";
+// import { Helmet } from "react-helmet";
+// import Iframe from "react-iframe";
 import { SketchComponent } from "../components/SketchComponent";
+import { Link } from "../components/Link";
 
 export const SBT_CONTRACT_ADDRESSES = ["0x60576A64851C5B42e8c57E3E4A5cF3CF4eEb2ED6"];
 
@@ -15,10 +16,22 @@ interface AddressPageProps {
 }
 
 const AddressPage: NextPage<AddressPageProps> = ({ nfts }) => {
-  const iframeSource = "https://opensea.io/";
   return (
     <Layout>
-      <SketchComponent nfts={nfts} />
+      <VStack>
+        <SketchComponent nfts={nfts} />
+        <Link to="https://mumbai.polygonscan.com/address/0xE134eC23f5e760dEC2689c67108F181E4f76E75E#writeContract">
+          <Text
+            fontSize={"xs"}
+            fontWeight={"medium"}
+            align="center"
+            bgGradient="linear(to-tr, teal.300,yellow.400)"
+            bgClip="text"
+          >
+            Mint your SBT mediator in PolygonScan (send possess tx)
+          </Text>
+        </Link>
+      </VStack>
     </Layout>
   );
 };
