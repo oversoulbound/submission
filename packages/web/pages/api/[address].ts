c0e8c0bb09f176baec9eb0d5526163e882ddb49f
Taiju Sanagi <taijusanagi@gmail.com>
2022-07-03 12:57:04 +0900

    forgot to commit

--- packages/web/pages/api/[address].ts
+++ packages/web/pages/api/[address].ts
@@ -1,13 +1,31 @@
+import Cors from "cors";
 import { getNftsForOwner } from "@alch/alchemy-sdk";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 import { alchemy } from "../../lib/alchemy";
 import { NFT } from "../../types/nft";
 
+const cors = Cors({
+  methods: ["GET", "HEAD"],
+});
+
+const runMiddleware = (req: NextApiRequest, res: NextApiResponse, fn: Cors) => {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: unknown) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+};
+
 export const SBT_CONTRACT_ADDRESS = "0x60576A64851C5B42e8c57E3E4A5cF3CF4eEb2ED6";
 
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  await runMiddleware(req, res, cors);
+
   const { address } = req.query;
   if (typeof address != "string") {
     return res.status(500).send({ success: false });
